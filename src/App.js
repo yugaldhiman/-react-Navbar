5bc40e21494e49956ecafc23da2d3aa24bb13ebb
yugal dhiman <yugaldhiman14@gmail.com>
2024-02-15 14:38:04 +0530

    fix new bug

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
-// import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 function App() {
   return (
     <>
-      {/* <Navbar title="TextUtils" /> */}
       <div className="container">
-        <TextForm heading="Enter the text" />
+        <TextForm heading="Enter the text to analyze"/>
       </div>
     </>
   );
